81f9c1e9d95525f3b0d48bf75f8684820a04b9b0
Main Key Store <44349800+zaffuto@users.noreply.github.com>
2023-02-04 23:45:31 -0300

    V1.1
    V1.1

--- app.js
+++ app.js
@@ -3,83 +3,51 @@ const {
     createProvider,
     createFlow,
     addKeyword,
-} = require('@bot-whatsapp/bot')
+    } = require('@bot-whatsapp/bot')
+    
+    const QRPortalWeb = require('@bot-whatsapp/portal')
+    const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+    const MockAdapter = require('@bot-whatsapp/database/mock')
+    
+    //-----
 
-const QRPortalWeb = require('@bot-whatsapp/portal')
-const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const MockAdapter = require('@bot-whatsapp/database/mock')
+    const flowMenu = addKeyword(['inicio','menu','hola'])
+        .addAnswer(
+        '🙌 Bienvenido al bot de Gregoria Cocina')
+        .addAnswer(
+        [        
+        'marca 👉 1 Para obtener la carta de terraza en PDF',
+        'marca 👉 2 Para conocer la dirección y horarios',
+        'marca 👉 3 Para pedir delivery o retirar en el local',
+        '#EL VERDADERO RINCÓN 🇦🇷 EN SANTIAGO',
+        ], 
+    );
+ 
+    const flowMenuPDF = addKeyword('1')
+        .addAnswer('Nuestra Carta Menu de terraza',
+        {
+            media: 'https://botchile.000webhostapp.com/GREGORIA-COCINA.pdf',
+        })
+        .addAnswer('no necesitás reservar mesa!',);
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer([
-    '📄 Aquí tenemos el flujo secundario',
-])
+    const flowLocal = addKeyword('2').addAnswer(
+        'Estamos en Vitacura!',
+        ).addAnswer('AV Padre Hurtado Nº 1376, esquina Las Hualtatas.',
+        ).addAnswer('Martes a Sabado de 10.30 a las 19.30 hs.',
+        ).addAnswer('Domingos de 11.30 a 14 hs y los Lunes descansamos',
+      
+    );
+      const flowDelivery = addKeyword('3').addAnswer(
+          'Pedí con este link https://mipedido.gregoria.cl/pedir',
+          ).addAnswer('Nuestro sitio web permite pedir a domicilio o retirar en el local',
+          ).addAnswer('Si es tu primera compra, podés canjear los descuentos',
+    );
 
-const flowDocs = addKeyword([
-    'doc',
-    'documentacion',
-    'documentación',
-]).addAnswer(
-    [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+    //    ----
 
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    [
-        '🤪 Únete al discord',
-        'https://link.codigoencasa.com/DISCORD',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
-    .addAnswer(
-        [
-            'te comparto los siguientes links de interes sobre el proyecto',
-            '👉 *doc* para ver la documentación',
-            '👉 *gracias*  para ver la lista de videos',
-            '👉 *discord* unirte al discord',
-        ],
-        null,
-        null,
-        [flowDocs, flowGracias, flowTuto, flowDiscord]
-    )
-
-const main = async () => {
+    const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    const adapterFlow = createFlow([flowMenu,flowMenuPDF,flowDelivery,flowLocal])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
@@ -90,5 +58,4 @@ const main = async () => {
 
     QRPortalWeb()
 }
-
 main()
